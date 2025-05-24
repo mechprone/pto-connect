9f2ca1e80ead4158eebc3e2962e74c389093c06f
mechprone <mvalzan@gmail.com>
2025-05-24 07:57:41 -0500

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./*.html', './*.js'],
-  theme: { extend: {} },
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
   plugins: [],
-};
+}
