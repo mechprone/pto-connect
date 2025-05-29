140d811d5af3215e0349d0dd1cea09de1c2c7dfb
mechprone <mvalzan@gmail.com>
2025-05-29 15:06:19 -0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
+import EventsDashboard from './pages/EventsDashboard'
+import CreateEvent from './pages/CreateEvent'
+import CreateFundraiser from './pages/CreateFundraiser'
 
 export default function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<div className="text-blue-600 p-4">PTO Central is running!</div>} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/" element={<div className="text-blue-600 p-4">PTO Central is running!</div>} />
+        <Route path="/events" element={<EventsDashboard />} />
+        <Route path="/events/create" element={<CreateEvent />} />
+        <Route path="/fundraisers/create" element={<CreateFundraiser />} />
       </Routes>
     </Router>
   )
