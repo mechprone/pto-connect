a998988d5197023ca123fff8d5ad174df567ceb3
mechprone <mvalzan@gmail.com>
2025-05-31 09:22:50 -0500

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path'; // 👈 add this
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'), // 👈 this enables @/ to mean /src
+    },
+  },
   server: {
     port: 3000,
     proxy: {
@@ -10,7 +16,7 @@ export default defineConfig({
         target: 'https://api.ptoconnect.com',
         changeOrigin: true,
         secure: true,
-      }
-    }
-  }
+      },
+    },
+  },
 });
