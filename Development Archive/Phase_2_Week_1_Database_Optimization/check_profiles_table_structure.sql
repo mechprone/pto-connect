650658a7a581ce691e62633aba63d437bd8acb17
mechprone <mvalzan@gmail.com>
2025-06-13 10:37:15 -0500

    v1.7.3: Monthly Reconciliation Module Phase 2 & Dashboard Navigation Fix COMPLETE

--- Development Archive/Phase_2_Week_1_Database_Optimization/check_profiles_table_structure.sql
+++
@@ -1,8 +0,0 @@
--- Check the actual structure of the profiles table
-SELECT column_name, data_type, is_nullable
-FROM information_schema.columns 
-WHERE table_name = 'profiles'
-ORDER BY ordinal_position;
-
--- Also check a sample of data to see what's actually there
-SELECT * FROM profiles LIMIT 3;
--- Development Archive/src/modules/communications/pages/SmsComposer.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-export default function SmsComposer() {
-  return (
-    <div className="p-4">
-      <h1 className="text-xl font-bold">SMS Composer (Coming Soon)</h1>
-    </div>
-  );
-}
